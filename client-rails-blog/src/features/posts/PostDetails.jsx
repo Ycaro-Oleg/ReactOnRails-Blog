58ef71b109ae48dd16c201a984a99777ef35e7b8
Ycaro-Oleg <ycaro.oleg@gmail.com>
2024-10-05 15:47:42 -0300

    Refactored the link titles to actually link to the post itself

--- client-rails-blog/src/features/posts/PostDetails.jsx
+++ client-rails-blog/src/features/posts/PostDetails.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import { useParams, useNavigate, Link} from "react-router-dom";
 import { API_URL } from "../../constants";
--- client-rails-blog/src/features/posts/PostList.jsx
+++ client-rails-blog/src/features/posts/PostList.jsx
@@ -1,6 +1,6 @@
 // API_URL comes from the .env.development file
 import { useState, useEffect } from "react";
-import React from "react";
+import { Link } from "react-router-dom";
 import { API_URL } from "../../constants"
 
 function PostList() {
@@ -26,11 +26,16 @@ function PostList() {
       }
       loadPosts()
     }, [])
+
   return(
     <div>
       {posts.map((post) => (
         <div key={post.id} className="post-container">
-          <h2>{post.title}</h2>
+          <h2>
+            <Link to = {`/posts/${post.id}`} className="post-title">
+              {post.title}
+            </Link>
+          </h2>
           <p>{post.body}</p>
         </div>
       ))}
