43748c4dae5a20586c4d50a076c33ee49c5dd092
Ycaro-Oleg <ycaro.oleg@gmail.com>
2024-10-05 14:18:00 -0300

    Added the vite for reactjs frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Da onde vem meus requests.
-    origins "http://127.0.0.1:5173"
+    origins "http://localhost:5173"
     # origins "outrodominio.com " -- Fazer isso depois
     resource "*",
       headers: :any,
